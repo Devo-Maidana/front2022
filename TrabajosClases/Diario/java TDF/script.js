85abc9e513237315ed8715b2fe733e4a22b0be7e
Devo <devoramaidana@tdf.edu.ar>
2022-07-25 15:33:12 -0300

    actividad mostrar 3 input

--- TrabajosClases/Diario/java TDF/script.js
+++ TrabajosClases/Diario/java TDF/script.js
@@ -4,7 +4,10 @@
 
 let btn_nombre =document.getElementById("btn_nombre");
 btn_nombre.addEventListener("click",mostrar );
-
+let btn_apellido =document.getElementById("btn_apellido");
+btn_apellido.addEventListener("click",mostrar );
+let btn_direccion =document.getElementById("btn_direccion");
+btn_direccion.addEventListener("click",mostrar );
 
 
 
@@ -12,6 +15,16 @@ function mostrar(){
     let nombre = "anonimo";
     let inputNombre= document.getElementById("inputNombre");
     nombre = inputNombre.value;
-    alert("Hola bienvenido " + nombre);
+    alert("Hola bienvenido " + nombre );
+
+    let apellido = "anonimo";
+    let inputApellido= document.getElementById("inputApellido");
+    apellido = inputApellido.value;
+    alert("Hola bienvenido " + apellido);
+
+    let direccion = "anonimo";
+    let inputDireccion= document.getElementById("inputDireccion");
+    direccion = inputDireccion.value;
+    alert("Hola bienvenido " + direccion);
 
 }
