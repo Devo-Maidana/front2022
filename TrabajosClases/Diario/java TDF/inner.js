0118f5f5a6e1d7995754040543b0f269c715d111
Devo <devoramaidana@tdf.edu.ar>
2022-08-01 22:29:48 -0300

    actividad lean

--- TrabajosClases/Diario/java TDF/inner.js
+++ TrabajosClases/Diario/java TDF/inner.js
@@ -1,25 +1,36 @@
-// Agrega elementos a la lista
 
-let botonLista = document.getElementById("botonLista");
-let lista = document.getElementById("lista");
-let inputLista = document.getElementById("inputLista");
+
+let botonLista= document.getElementById("botonLista");
+botonLista.addEventListener("click",agregarElemento);
+
+
+
 
 function agregarElemento(){
-    let elemento = document.createElement("li");
-    elemento.innerHTML = inputLista.value;
-    lista.appendChild(elemento);
+    
+let lista = document.getElementById("lista");
+let inputLista = document.getElementById("inputLista");
+let elemento = document.createElement("li");
+elemento.innerHTML = inputLista.value;
+lista.appendChild(elemento);
+
 }
 
-// agrega parrafo al div
 
 let botonParrafo = document.getElementById("botonParrafo");
+botonParrafo.addEventListener("click",agregarParrafo);
+
+let  divParrafo = document.getElementById("divParrafo");
+let inputParrafo = document.getElementById ("inputParrafo");
 
 function agregarParrafo(){
-    let divParrafo = document.getElementById("divParrafo");
+
+   
+   
     let parrafo = document.createElement("p");
-    let inputParrafo = document.getElementById("inputParrafo");
+    
     parrafo.innerHTML = inputParrafo.value;
     divParrafo.appendChild(parrafo);
-}
 
+}
 
