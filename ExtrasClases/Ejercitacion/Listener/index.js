3799febaa0d9dd4068c9e62380cd445d97d728cb
Devo <devoramaidana@tdf.edu.ar>
2022-08-02 22:38:26 -0300

    clase braian togglear

---
+++ ExtrasClases/Ejercitacion/Listener/index.js
@@ -0,0 +1,10 @@
+let botonTogglear = document.getElementById(botonTogglear);
+botonTogglear.addEventListener("click,Ftogglear");
+
+
+function Ftogglear (){
+
+let element = document.querySelector(".div")
+element.classList.togglear("ocultar");
+
+}
